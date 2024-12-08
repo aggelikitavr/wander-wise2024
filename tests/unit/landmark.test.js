17a06e30139e99ed2ef5dcc1f80ed20430b0f430
GEORGE RIZOS <81235981+grrizos@users.noreply.github.com>
2024-12-08 15:17:04 +0200

    Update landmark.test.js

--- tests/unit/landmark.test.js
+++ tests/unit/landmark.test.js
@@ -5,23 +5,21 @@ const { setup, teardown } = require('../helpers/setup.js');
 test.before(setup);
 test.after.always(teardown);
 
-test('POST /landmarks creates a new Landmark', async (t) => {
+// Test for DELETE /landmarks/:landmarkId
+test('DELETE /landmarks/:landmarkId deletes a landmark', async (t) => {
     const { got } = t.context;
-    const newLandmark = {
-        "id": 4,
-        "name": "Buda Castle",
-        "details": "Buda Castle is a historic royal palace in Budapest, Hungary",
-        "location": [
-            "47.4979° N",
-            "19.0399° E"
-        ],
-    }
 
-    const response = await got.post('landmarks', { json: newLandmark });
-    t.is(response.statusCode, 200);
-    t.is(response.body.id, 4);
-    t.is(response.body.name, 'Buda Castle');
-    t.is(response.body.details, 'Buda Castle is a historic royal palace in Budapest, Hungary');
-    t.is(response.body.location.length, 2);
-    t.deepEqual(response.body.location, ['47.4979° N', '19.0399° E']);
+    const landmarkId = 4;
+
+    // Make the DELETE request
+    const response = await got.delete(`landmarks/${landmarkId}`);
+
+    console.log(response.body);
+
+    // Validate response
+    t.is(response.statusCode, 200); // Expecting No Content
+
+    // Confirm deletion
+    // const fetchResponse = await got(`landmarks/${landmarkId}`, { throwHttpErrors: false });
+    // t.is(fetchResponse.statusCode, 400); // Not Found
 });
