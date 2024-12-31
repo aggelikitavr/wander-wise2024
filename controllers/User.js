3faf6e28269ed1e9216c99befba776692ad06715
VlachakisTheodoros <vlahakis.gr@gmail.com>
2024-12-31 17:21:27 +0200

    Fix all violations in controllers/User.js

--- controllers/User.js
+++ controllers/User.js
@@ -3,72 +3,86 @@
 var utils = require('../utils/writer.js');
 var User = require('../service/UserService');
 
-module.exports.createUser = function createUser (req, res, next, body) {
+module.exports.createUser = function createUser (_, res, next, body) {
   User.createUser(body)
     .then(function (response) {
       utils.writeJson(res, response);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response);
+      next();
     });
 };
 
-module.exports.createUsersWithListInput = function createUsersWithListInput (req, res, next, body) {
+module.exports.createUsersWithListInput = function createUsersWithListInput (_, res, next, body) {
   User.createUsersWithListInput(body)
     .then(function (response) {
       utils.writeJson(res, response);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response);
+      next();
     });
 };
 
-module.exports.deleteUser = function deleteUser (req, res, next, username) {
+module.exports.deleteUser = function deleteUser (_, res, next, username) {
   User.deleteUser(username)
     .then(function (response) {
       utils.writeJson(res, response);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response);
+      next();
     });
 };
 
-module.exports.getUserByName = function getUserByName (req, res, next, username) {
+module.exports.getUserByName = function getUserByName (_, res, next, username) {
   User.getUserByName(username)
     .then(function (response) {
       utils.writeJson(res, response);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response);
+      next();
     });
 };
 
-module.exports.loginUser = function loginUser (req, res, next, username, password) {
+module.exports.loginUser = function loginUser (_, res, next, username, password) {
   User.loginUser(username, password)
     .then(function (response) {
       utils.writeJson(res, response);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response);
+      next();
     });
 };
 
-module.exports.logoutUser = function logoutUser (req, res, next) {
+module.exports.logoutUser = function logoutUser (_, res, next) {
   User.logoutUser()
     .then(function (response) {
       utils.writeJson(res, response);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response);
+      next();
     });
 };
 
-module.exports.updateUser = function updateUser (req, res, next, body, username) {
+module.exports.updateUser = function updateUser (_, res, next, body, username) {
   User.updateUser(body, username)
     .then(function (response) {
       utils.writeJson(res, response);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response);
+      next();
     });
 };
