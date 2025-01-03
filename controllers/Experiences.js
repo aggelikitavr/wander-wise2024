988f434810e0b283258719a1f3098331757fe52a
aggelikitavr <151464403+aggelikitavr@users.noreply.github.com>
2025-01-03 16:35:05 +0200

    Update Experiences.js
    Removed some unused variables

--- controllers/Experiences.js
+++ controllers/Experiences.js
@@ -3,42 +3,50 @@
 var utils = require('../utils/writer.js');
 var Experiences = require('../service/ExperiencesService');
 
-module.exports.createExperience = function createExperience (req, res, next, body) {
+module.exports.createExperience = function createExperience (_, res, next, body) {
   Experiences.createExperience(body)
     .then(function (response) {
       utils.writeJson(res, response, 201);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response, 400);
+      next();
     });
 };
 
-module.exports.deleteExperience = function deleteExperience (req, res, next, experienceId) {
+module.exports.deleteExperience = function deleteExperience (_, res, next, experienceId) {
   Experiences.deleteExperience(experienceId)
     .then(function (response) {
-      utils.writeJson(res, response,200);
+      utils.writeJson(res, response, 200);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response,400);
+      next();
     });
 };
 
-module.exports.getExperienceById = function getExperienceById (req, res, next, experienceId) {
+module.exports.getExperienceById = function getExperienceById (_, res, next, experienceId) {
   Experiences.getExperienceById(experienceId)
     .then(function (response) {
       utils.writeJson(res, response, 200);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response, 400);
+      next();
     });
 };
 
-module.exports.updateExperience = function updateExperience (req, res, next, body, landmarkId, experienceId) {
+module.exports.updateExperience = function updateExperience (_, res, next, body, landmarkId, experienceId) {
   Experiences.updateExperience(body, landmarkId, experienceId)
     .then(function (response) {
       utils.writeJson(res, response, 200);
+      next();
     })
     .catch(function (response) {
       utils.writeJson(res, response, 400);
+      next();
     });
 };
