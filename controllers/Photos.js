21f9d21c0e8de5df73a8ba0b0754c2e32a12f3bf
aggelikitavr <151464403+aggelikitavr@users.noreply.github.com>
2025-01-04 02:22:37 +0200

    Update Photos.js
    Fixed some violations related to unused variables and added more comments to improve documentation and better understanding of the code.

--- controllers/Photos.js
+++ controllers/Photos.js
@@ -3,32 +3,44 @@
 var utils = require('../utils/writer.js');
 var Photos = require('../service/PhotosService');
 
-module.exports.addPhoto = function addPhoto (req, res, next, body, landmarkId) {
+// This function is used in the POST/landmarks/{landmarkId}/photos endpoint to add a new photo
+// for a specific landmark using landmarkId
+module.exports.addPhoto = function addPhoto (_, res, body, landmarkId) {
   Photos.addPhoto(body, landmarkId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 201); // Photo added successfully
+      next();
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 400); // Error, photo not added
+      next();
     });
 };
 
-module.exports.deletePhoto = function deletePhoto (req, res, next, landmarkId, photoId) {
+// This function is used in the DELETE/landmarks/{landmarkId}/photos/{photoId} endpoint to delete a photo
+// of a specific landmark using landmarkId and photoId
+module.exports.deletePhoto = function deletePhoto (_, res, landmarkId, photoId) {
   Photos.deletePhoto(landmarkId, photoId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200); // Photo deleted successfully
+      next();
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 400); // Error, photo not deleted
+      next();
     });
 };
 
-module.exports.getPhotosForLandmark = function getPhotosForLandmark (req, res, next, landmarkId) {
+// This function is used in the GET/landmarks/{landmarkId}/photos endpoint to retrieve photos
+// of a specific landmark using landmarkId
+module.exports.getPhotosForLandmark = function getPhotosForLandmark (_, res, landmarkId) {
   Photos.getPhotosForLandmark(landmarkId)
     .then(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 200); // Photo retrieved successfully
+      next();
     })
     .catch(function (response) {
-      utils.writeJson(res, response);
+      utils.writeJson(res, response, 400); // Error, photo not retrieved
+      next();
     });
 };
