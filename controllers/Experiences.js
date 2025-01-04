33645d2b1bb57b6aee82dce6c41dff338ff5045e
aggelikitavr <151464403+aggelikitavr@users.noreply.github.com>
2025-01-04 02:23:25 +0200

    Update Experiences.js
    Improved some comments

--- controllers/Experiences.js
+++ controllers/Experiences.js
@@ -16,7 +16,7 @@ module.exports.createExperience = function createExperience (_, res, next, body)
     });
 };
 
-// This function is used in the DELETE/experiences/{experienceId} endpoint and deletes an experience based on experienceId
+// This function is used in the DELETE/experiences/{experienceId} endpoint and deletes an experience using experienceId
 module.exports.deleteExperience = function deleteExperience (_, res, next, experienceId) {
   Experiences.deleteExperience(experienceId)
     .then(function (response) {
@@ -29,7 +29,7 @@ module.exports.deleteExperience = function deleteExperience (_, res, next, exper
     });
 };
 
-// This function is used in the GET/experiences/{experienceId} endpoint and retrieves an existing experience based on experienceId
+// This function is used in the GET/experiences/{experienceId} endpoint and retrieves an existing experience using experienceId
 module.exports.getExperienceById = function getExperienceById (_, res, next, experienceId) {
   Experiences.getExperienceById(experienceId)
     .then(function (response) {
@@ -42,10 +42,11 @@ module.exports.getExperienceById = function getExperienceById (_, res, next, exp
     });
 };
 
-// This function is used in the PUT/experiences/{experienceId}/{landmarkId} endpoint and updates an existing experience based on experienceId and landmarkId
-module.exports.updateExperience = function updateExperience(_, res, next, body, context) {
-  // context: { landmarkId, experienceId }
-  Experiences.updateExperience(body, context)
+// This function is used in the PUT/experiences endpoint to update an existing experience
+module.exports.updateExperience = function updateExperience(_, res, next, body, data) {
+  const { landmarkId, experienceId } = data;
+
+  Experiences.updateExperience(body, landmarkId, experienceId)
     .then(function (response) {
       utils.writeJson(res, response, 200); // Experience updated successfully
       next();
