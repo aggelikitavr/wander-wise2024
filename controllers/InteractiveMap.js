6e4aef30d5683c851bbd71a53834c9bfc85b12f2
GEORGE <georgeriz007@gmail.com>
2025-01-06 21:29:34 +0200

    changes

--- controllers/InteractiveMap.js
+++ controllers/InteractiveMap.js
@@ -1,14 +1,25 @@
 'use strict';
 
+// Required dependencies
 var utils = require('../utils/writer.js');
 var InteractiveMap = require('../service/InteractiveMapService');
 
-module.exports.interactive_map = function interactive_map (req, res, next) {
+/**
+ * Handles the interactive map request
+ * 
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ * @param {Function} next - The next middleware function
+ */
+module.exports.interactive_map = function interactive_map(req, res, next) {
+  // Call the service function and handle the promise response
   InteractiveMap.interactive_map()
-    .then(function (response) {
+    .then(function(response) {
+      // On success, write the JSON response
       utils.writeJson(res, response);
     })
-    .catch(function (response) {
+    .catch(function(response) {
+      // On error, write the error response as JSON
       utils.writeJson(res, response);
     });
 };
--- cypress.config.js
+++ cypress.config.js
@@ -3,7 +3,13 @@ const { defineConfig } = require("cypress");
 module.exports = defineConfig({
   e2e: {
     setupNodeEvents(on, config) {
-      // implement node event listeners here
+      // Placeholder for future node event listeners
+      // Example: on('before:run', () => { console.log('Test run is starting'); });
     },
+    baseUrl: "http://localhost:8060", // Set a base URL for your application
+    specPattern: "cypress/e2e/**/*.cy.js", // Define the pattern for spec files
+    supportFile: "cypress/support/e2e.js", // Specify the support file location
+    viewportWidth: 1280, // Set default viewport width
+    viewportHeight: 720, // Set default viewport height
   },
 });
