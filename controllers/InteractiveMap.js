0aaa3485efce64ef928ae8beddaf4df2781bf980
GEORGE <georgeriz007@gmail.com>
2025-01-07 20:28:22 +0200

    almost

--- controllers/InteractiveMap.js
+++ controllers/InteractiveMap.js
@@ -15,12 +15,12 @@ const InteractiveMap = require('../service/InteractiveMapService');
  * @function interactive_map
  * @param {Object} _ - The HTTP request object (unused in this implementation).
  * @param {Object} res - The HTTP response object used to send the response.
- * @param {Function} _next - The next middleware function in the pipeline (unused).
+ * @param {Function} _ - The next middleware function in the pipeline (unused).
  * @returns {void}
  * @description Calls the `interactive_map` service function and processes the returned promise. 
  * On success, sends the interactive map data as JSON. On failure, sends the error response as JSON.
  */
-module.exports.interactive_map = function interactive_map(_, res, _next) {
+module.exports.interactive_map = function interactive_map(_, res, _) {
   InteractiveMap.interactive_map()
     .then(function (response) {
       // Write the successful response as JSON
