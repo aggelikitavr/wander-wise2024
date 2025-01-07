5a97fa6943ed7325703595b7074e8b413ecce1e5
GEORGE <georgeriz007@gmail.com>
2025-01-07 18:07:52 +0200

    almost

--- service/CommentsService.js
+++ service/CommentsService.js
@@ -9,14 +9,14 @@
  * Create a comment for a specific review of a landmark.
  * 
  * @function addComment
- * @param {Object} _ - Unused request body containing the comment details.
+ * @param {Object} requestBody - (Optional) The request body containing the comment details.
  * @param {number} landmarkId - The unique identifier of the landmark. Use `348` for testing.
  * @param {number} reviewId - The unique identifier of the review. Use `23` for testing.
  * @returns {Promise<void>} A promise that resolves with no response value for this operation.
  * @description Adds a new comment to a specified review of a landmark. 
  * This function is for testing purposes and currently does not perform any actual operation.
  */
-exports.addComment = function (_, landmarkId, reviewId) {
+exports.addComment = function (requestBody, landmarkId, reviewId) {
   return new Promise(function (resolve) {
     // Simulating successful operation for testing purposes
     resolve();
@@ -29,12 +29,12 @@ exports.addComment = function (_, landmarkId, reviewId) {
  * @function deleteComment
  * @param {number} landmarkId - The unique identifier of the landmark. Use `348` for testing.
  * @param {number} reviewId - The unique identifier of the review. Use `23` for testing.
- * @param {number} _ - Unused unique identifier of the comment. Use `2` for testing.
+ * @param {number} commentId - (Optional) The unique identifier of the comment. Use `2` for testing.
  * @returns {Promise<void>} A promise that resolves with no response value for this operation.
  * @description Deletes a specified comment from a review of a landmark. 
  * This function is for testing purposes and currently does not perform any actual operation.
  */
-exports.deleteComment = function (landmarkId, reviewId, _) {
+exports.deleteComment = function (landmarkId, reviewId, commentId) {
   return new Promise(function (resolve) {
     // Simulating successful operation for testing purposes
     resolve();
